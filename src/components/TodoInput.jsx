4c7e7665da3077c2d86052ac92805cee6ff61d06
Shaisab Mistry <shaisabmistry@gmail.com>
2024-10-05 18:48:36 -0400

    Click "Enter" to add items

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,21 +1,37 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 
 export default function TodoInput(props) {
     const {handleAddTodos, setTodoValue, todoValue} = props
+
+    useEffect(() => {
+        const keyDownHandler = event => {
+            if (event.key === 'Enter') {
+                handleAddTodos(todoValue)
+                setTodoValue('')
+
+            }
+        }
+        document.body.addEventListener("keydown", keyDownHandler)
+        return () => {
+            document.body.removeEventListener('keydown', keyDownHandler)
+        }
+
+    }, [todoValue, handleAddTodos])
 
     return (
         <header>
             <input value={todoValue} onChange={(e) => {
                 setTodoValue(e.target.value)
             }} placeholder={'Enter todo...'}/>
-            <button onClick={()=> {
+            <button onClick={() => {
                 handleAddTodos(todoValue)
                 setTodoValue('')
 
-            }}>Add</button>
-
+            }}>Add
+            </button>
         </header>
     )
+
 
 }
